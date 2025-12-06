7ab542907892f48441f146f7e433e46bda39d7e9
Sayan2713 <sayanmonadl619@gmail.com>
2025-12-06 20:07:58 +0530

    Fix Invitation

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for our backend
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://tripsplit-api.onrender.com';
 
 // --- Axios Global Setup ---
 const api = axios.create({
